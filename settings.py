0b5a90642caefe32cf6b6d63e335e816c9a78dc3
Vijayamanikanta Jampani <109064376+stromen970@users.noreply.github.com>
2024-02-01 12:13:40 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+#Add our app in inside Installed_apps "My app name is 'testapp'"
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'testapp',
+]
